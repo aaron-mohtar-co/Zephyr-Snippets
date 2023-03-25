d6ff7a75d70bc9d00cdb94154bac35959a275b96
Craig Peacock <craigp@beyondlogic.org>
2023-03-25 16:20:52 +1030

    Add Zephyr Logging Subsystem Example

---
+++ Logging/src/main.c
@@ -0,0 +1,32 @@
+/*
+ * Zephyr Logging Subsystem Example. 
+ * Demonstrates the use of error, informational, warning and debug 
+ * level logging
+ * 
+ * Copyright (c) 2023 Aaron Mohtar & Co Pty Ltd
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+// If log level not specifed, use default log level:
+LOG_MODULE_REGISTER(logging);
+// LOG_MODULE_REGISTER(logging, LOG_LEVEL_DBG);
+
+void main(void)
+{
+	printk("Zephyr Logging Example\nBoard: %s\n", CONFIG_BOARD);
+
+	while (1) {
+
+		LOG_ERR("This is an error");
+		LOG_INF("This is informational");
+		LOG_WRN("This is a warning");
+		LOG_DBG("This is for debugging");
+		k_msleep(1000);
+	}
+}
