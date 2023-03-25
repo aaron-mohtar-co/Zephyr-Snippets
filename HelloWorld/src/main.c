6bba9b75747c4a974f7326df76985dcbc0904c3b
Craig Peacock <craigp@beyondlogic.org>
2023-03-25 16:25:11 +1030

    Add hello world console example

---
+++ HelloWorld/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Zephyr Hello World Boiler Plate Example
+ * Demonstrates a basic Zephyr project with console support
+ * 
+ * Copyright (c) 2023 Aaron Mohtar & Co Pty Ltd
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+
+void main(void)
+{
+	while (1) {
+		printk("Hello World\n");
+		k_msleep(1000);
+	}
+}
